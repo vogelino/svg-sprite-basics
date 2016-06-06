a74af8bab559c0475d5c515e50374623fce15ce7
vogelino <contact@vogelino.com>
2016-06-06 23:28:07 +0200

    Added browser-sync to watch and serve files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# svg-sprite-basics
-Learning how to use SVG sprites in an automated fashion
+# SVG sprites - proof of concept
+Learning how to use SVG sprites in an automated fashion using:
+
+- [gulp](https://github.com/gulpjs/gulp) - To define build processes and automate operations
+- [gulp-svg-sprite](https://github.com/jkphl/gulp-svg-sprite) - To automatically create a svg sprite out of svg icons
+- [browser-sync](https://www.browsersync.io/docs/gulp/) - To quickly creating a devserver with automated reload and file injection
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 var spriteSvg = require('gulp-svg-sprite');
+var browserSync = require('browser-sync').create();
 
 const paths = {
 	src: {
 		icons: './src/icons/**/*.svg'
 	},
 	dist: {
+		base: './dist',
 		icons: './dist/icons'
 	}
 };
+
+gulp.task('serve', [ 'svg-sprite' ], () => {
+	browserSync.init({
+		server: {
+			baseDir: paths.dist.base
+		}
+	});
+
+	gulp.watch(paths.src.icons, [ 'svg-sprite' ]);
+	gulp.watch(`${paths.dist.base}/**/*`).on('change', browserSync.reload);
+});
 
 gulp.task('svg-sprite', () => gulp
 	.src(paths.src.icons)
@@ -21,6 +34,7 @@ gulp.task('svg-sprite', () => gulp
 		}
 	}))
 	.pipe(gulp.dest(paths.dist.icons))
+	.pipe(browserSync.stream())
 );
 
-gulp.task('default', [ 'svg-sprite' ]);
+gulp.task('default', [ 'serve' ]);
